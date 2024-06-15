134bc91a4413659297e6f9f86045f8ce26a7c01d
dbamfo <bamfodaniel@yahoo.com>
2024-06-15 21:55:05 +0100

    comment out terraform apply

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "name" {
-  type = list(string)
-  default = [ "lin","lau", "kay"," Nic" ]
+  type    = list(string)
+  default = ["lin", "lau", "kay", "nic"]
 }
 
- variable "ami" {
-    default = "ami-053a617c6207ecc7b"
-    type = string
-    description = "ami for the new env"
- }
+variable "ami" {
+  default     = "ami-053a617c6207ecc7b"
+  type        = string
+  description = "ami for the new env"
+}
 
- variable "instance_type" {
-  default =  "t2.small"
-  type = string
+variable "instance_type" {
+  default = "t2.small"
+  type    = string
 }
 
 variable "tag_name" {
--- version.tf
+++ version.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.37.0"
     }
   }
   backend "s3" {
-      bucket = "devops-s3573"
-      key    = "backend/statefile"
-      region = "eu-west-2"
+    bucket = "devops-s3573"
+    key    = "backend/statefile"
+    region = "eu-west-2"
   }
 }
 
